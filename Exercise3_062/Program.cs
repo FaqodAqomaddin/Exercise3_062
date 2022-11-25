77bddda2e76bff59c3616ce8208e3fcd94a48b58
FaqodAqomaddin <faqod.aqomaddin.ft21@mail.umy.ac.id>
2022-11-25 16:15:04 +0700

    Add Class Node

--- Exercise3_062/Program.cs
+++ Exercise3_062/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+
+namespace Exercise_Linked_List_C
+{
+    class Node
+    {
+        /*Node class respresent the node of doubly linked list
+         * it consists of the information part and link to
+         * its succeding and preceeding
+         * in term of next and previous */
+        public int noMhs;
+        public string name;
+        //point to the succeding node
+        public Node next;
+        public Node prev;
+    }
+}
